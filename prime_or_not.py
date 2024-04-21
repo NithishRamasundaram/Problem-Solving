b7439cdfb700b9fea1a9e97ca465d28e64e4a3a2
[nithish r] <rnithi666@gmail.com>
2024-04-21 09:17:21 +0530

    practices

---
+++ prime_or_not.py
@@ -0,0 +1,12 @@
+num=int(input("Enter the number"))
+if num>1:
+    for i in range(2,num):
+        if(num%i==0):
+            k=0
+        else:
+            k=1
+
+if k==0:
+    print("Non prime")
+else:
+    print("Prime number")
