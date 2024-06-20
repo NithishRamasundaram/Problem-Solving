c4193c32ba01b1ee3e55db287d6ccc7968700f75
[nithish r] <rnithi666@gmail.com>
2024-06-20 06:49:24 +0530

    updated program

--- prime_number.py
+++ prime_number.py
@@ -6,7 +6,6 @@ while temp > 0:
     rem = temp % 10
     rev = (rev * 10) + rem
     temp = temp // 10
-
 if num == rev:
     print("Palindrome")
 else:
--- star.py
+++ star.py
@@ -1,4 +1,10 @@
-for i in range(1,7):
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
+num = 79
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
