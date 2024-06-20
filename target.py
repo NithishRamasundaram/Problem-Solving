7e6ff42ebf96a40d0ef6a06737f57a3f15aa6183
[nithish r] <rnithi666@gmail.com>
2024-06-20 12:42:29 +0530

    updated target file

--- target.py
+++ target.py
@@ -1,12 +1,8 @@
-num=int(input("Number:"))
-temp=num
-rev=0
-while temp>0:
-    rem=temp%10
-    rev=(rev*10)+rem
-    temp=temp//10
-if rev == num:
-    print("Palindrome")
-else:
-    print("Not a Palindrome")
-
+n=int(input("enter number:"))
+f=1
+if n==0:
+    print("factorial is",f)
+elif n > 0:
+    for i in range(2,n+1):
+        f=f*i
+print("The factorial is ",f)
