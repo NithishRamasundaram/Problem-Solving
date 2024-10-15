49a168c9d5e5bd46fd728c2e2828cc504129da57
[nithish r] <rnithi666@gmail.com>
2024-10-15 14:12:34 +0530

    changes updated

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
-num=int(input("Enter the number:"))
-if num <0:
-    print("INVALID")
-elif num ==0:
-    print("the value for zero is 1:")
+n=int(input("Enter the number:"))
+if n<0:
+    print("Invalid")
+elif n==0:
+    print("The value for zero is 1")
 else:
-    for i in range(1,num):
-        num=num*i
-    print(num)
+    for i in range(1,n):
+        n=n*i
+    print(n)
