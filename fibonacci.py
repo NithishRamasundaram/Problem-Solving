e54793b753b7c6ddf4577ea61009300485e414f0
[nithish r] <rnithi666@gmail.com>
2024-10-15 14:27:56 +0530

    updated

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,5 @@
-num=int(input("enter the number:"))
-n1=0
-n2=1
-count=0
-while count < num:
-    print(n1)
-    c=n1+n2
-    n1=n2
-    n2=c
-    count+=1
-
+n=int(input("Enter the number:"))
+a, b = 0, 1
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
