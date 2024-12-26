0606cc39e565f39e3d0ae349bd9282457ae20f27
[nithish r] <rnithi666@gmail.com>
2024-12-26 16:12:20 +0530

    Armstrong number updated

--- armstrong.py
+++ armstrong.py
@@ -1,11 +1,11 @@
-num=int(input("Enter the number:"))
-temp=num
+n=int(input("Enter Amstrong number:"))
+temp=n
 rev=0
-while temp>0:
-    rem=temp%10
-    rev=rev+rem**3
-    temp//=10
-if num==rev:
-    print("ARMSTRONG NUMBER")
+while n > 0:                        # Loop runs until `temp` becomes 0.
+    rem = n % 10                    # Get the last digit of the number.
+    rev = rev + rem**3              # Add the cube of the last digit to `rev`.
+    n //= 10                        # Remove the last digit from `temp` by performing integer division.
+if temp==rev:
+    print("Given number is Amstrong number")
 else:
-    print("not a armstrong number")
+    print("Giver number is not an Amstrong number")
