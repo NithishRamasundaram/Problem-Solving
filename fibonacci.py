f9b7dfc8597bf8aef6dec05b31ae1df75cfff682
[nithish r] <rnithi666@gmail.com>
2024-12-26 16:41:39 +0530

    Fibonacci updated with two logic of codes

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,17 @@
+n=int(input("Enter Fibo series number:"))
+a=0
+b=1
+print(a,b,end=" ")
+for i in range(2,n):
+    c=a+b
+    print(c,end=" ")
+    a=b
+    b=c
+
+'''
 n=int(input("Enter the number:"))
 a, b = 0, 1
 for i in range(n):
     print(a, end=" ")
     a, b = b, a + b
+'''
