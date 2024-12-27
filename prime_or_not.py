1d4cbc392dd9388de63b6e3100daf6c36060a586
[nithish r] <rnithi666@gmail.com>
2024-12-27 08:33:35 +0530

    Prime number is updated

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,9 +1,12 @@
 n=int(input("Enter the number:"))
+prime=1
 if n>1:
     for i in range(2,n):
-        if n % i==0:
-            print("Not a prime number")
-            break
-
-    else:
-        print("Prime number")
+        if n%i!=0:
+            pass
+        else:
+            prime=0
+if prime==1:
+    print("Given number is prime")
+else:
+    print("Given number is not a prime")
