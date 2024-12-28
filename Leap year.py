b70582dbc32831b10fe3f56613a05433d88e3742
[nithish r] <rnithi666@gmail.com>
2024-12-28 12:16:03 +0530

    Leap year program

--- Leap year.py
+++ Leap year.py
@@ -1,2 +1,5 @@
-n=int(input("Enter the year:"))
-if n%4==0
+n = int(input("Enter the year: "))
+if (n % 4 == 0 and n % 100 != 0) or (n % 400 == 0):
+    print("It is a leap year")
+else:
+    print("It is not a leap year")
